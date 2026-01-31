e17d083b82af518d208e7341c23943bd7523cd3e
lucasrivelo <lucasrivelocampos@gmail.com>
2026-01-31 16:14:47 -0400

    Types + ajustes na obrigatoriedade de atributos + verificação da dataE(Data Esperada de Devolução)

--- src/domain/entities/Rental.ts
+++ src/domain/entities/Rental.ts
@@ -1,16 +1,23 @@
 export class Rental{
-  id?: string;
+  id: string;
   Car_id: string;
   User_id: string;
   DataI: Date;
-  DataE?: Date;
+  DataE: Date;
   DataF?: Date;
 
-  constructor(id: string, Car_id: string, User_id: string, DataF: Date){
+  constructor(id: string, Car_id: string, User_id: string, DataE: Date){
     this.id = id;
     this.Car_id = Car_id;
     this.User_id = User_id;
     this.DataI = new Date(); 
-    this.DataF = DataF;
+    this.DataE = DataE;
+
+    const padrao = 24*60*60*1000;
+    const duracaoP = this.DataE.getTime() - this.DataI.getTime();
+
+    if (duracaoP<padrao){
+      throw new Error("Duração de Devolução Prevista menor do que o padrão (24 Horas).");
+    }
   }
 }
---
+++ src/infra/container/types.ts
@@ -0,0 +1,4 @@
+export const TYPES = {
+  ICarRepo: Symbol.for("ICarRepo"),
+  IRentalRepo: Symbol.for("IRentalRepo"),
+};
